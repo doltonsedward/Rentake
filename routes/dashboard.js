988234293774846fc33376a1e22799f6bf8e80ac
D3NCyan <edwarddoltons@gmail.com>
2021-12-13 07:52:27 +0800

    add router for admin/dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,4 +4,8 @@ router.get('/login', function(req, res) {
     res.render('auth/login-admin', { title: 'Login to dashboard', isLogin: req.session.isLogin})
 })
 
+router.get('/', function(req, res) {
+    res.render('admin/dashboard', { title: 'Dashboard', isLogin: req.session.isLogin})
+})
+
 module.exports = router
